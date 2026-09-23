279ce52b55308ebcb117317bf359c6ead015c558
Piyush Kumar <kmrpiyush95@gmail.com>
2026-09-23 07:16:39 +0530

    adding cancel status

--- src/main/java/com/bca/pratima/appenum/BookBorrowStatus.java
+++ src/main/java/com/bca/pratima/appenum/BookBorrowStatus.java
@@ -1,5 +1,5 @@
 package com.bca.pratima.appenum;
 
 public enum BookBorrowStatus {
-    SUBMITTED, PENDING, APPROVED, REJECTED, RETURNED
+    SUBMITTED, PENDING, APPROVED, REJECTED, RETURNED, CANCEL
 }
